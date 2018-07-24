1d087950e933a262fe577043327ec8a817d3614b
shlomograch <shlomegrach@gmail.com>
2018-07-24 09:16:28 -0600

    Add files via upload

---
+++ Scripts/ng-prism.js
@@ -0,0 +1,12 @@
+﻿angular.module('ngPrism', []).
+    directive('prism', [function () {
+            return {
+                restrict: 'A',
+                link: function ($scope, element, attrs) {
+                    element.ready(function () {
+                        Prism.highlightElement(element[0]);
+                    });
+                }
+            }
+        }]
+    );
